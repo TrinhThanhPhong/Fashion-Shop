4c681d493c63ba5b1e4a5e52c58fec3ad9582d7c
TrinhThanhPhong <trinhthanhphongzz@gmail.com>
2025-02-18 17:06:14 +0700

    Add chatbot

--- FashionShop/Hubs/ChatRealtime.cs
+++ FashionShop/Hubs/ChatRealtime.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace FashionShop.Hubs
@@ -10,6 +14,27 @@ namespace FashionShop.Hubs
     [HubName("chat")]
     public class ChatRealtime : Hub
     {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        public async Task Message(string name, string message)
+        {
+            Clients.All.message(name, message);
+
+            string aiResponse = await GetRasaResponse(message);
+            Clients.All.message("Bot", aiResponse);
+        }
+
+        private async Task<string> GetRasaResponse(string userMessage)
+        {
+            var payload = new { sender = "user", message = userMessage };
+            var jsonPayload = JsonConvert.SerializeObject(payload);
+            var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("http://localhost:5005/webhooks/rest/webhook", content);
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            dynamic result = JsonConvert.DeserializeObject(responseBody);
+            return result.Count > 0 ? result[0].text.ToString() : "Xin lỗi, tôi không hiểu!";
+        }
         public void Hello()
         {
             Clients.All.hello();
@@ -20,9 +45,9 @@ namespace FashionShop.Hubs
             Clients.Caller.connect(name);
         }
 
-        public void Message(string name, string message)
-        {
-            Clients.All.message(name, message);
-        }
+        //public void Message(string name, string message)
+        //{
+        //    Clients.All.message(name, message);
+        //}
     }
 }
