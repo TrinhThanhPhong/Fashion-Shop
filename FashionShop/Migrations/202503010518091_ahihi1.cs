a0a613897c13551f5a45ff78747ccccc62f296be
NTaam <tamzx1102@gmail.com>
2025-03-02 00:13:43 +0700

    push database

---
+++ FashionShop/Migrations/202503010518091_ahihi1.cs
@@ -0,0 +1,18 @@
+﻿namespace FashionShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ahihi1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Tb_Product", "ahihi", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Tb_Product", "ahihi");
+        }
+    }
+}
