cd99f5fbd33cea9b3e113fd4af7d267679141e8f
tran-kim-quang <107093325+tran-kim-quang@users.noreply.github.com>
2025-05-07 11:00:47 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,35 +1,22 @@
 import torch
 import torch.nn as nn
 
-# class IntentClassifier(nn.Module):
-#     def __init__(self, input_size, hidden_size, embedding_dim, num_classes):
-#         super(IntentClassifier, self).__init__()
-#         self.embedding = nn.Embedding(input_size, embedding_dim)
-#         self.rnn = nn.LSTM(embedding_dim, hidden_size, batch_first=True)
-#         self.dropout = nn.Dropout(0.3)
-#         self.fc = nn.Linear(hidden_size, num_classes)
-
-#     def forward(self, x):
-#         embedded = self.embedding(x)
-#         _, (hidden, _) = self.rnn(embedded)
-#         hidden = self.dropout(hidden[-1])
-#         output = self.fc(hidden)
-#         return output
-
 class IntentClassifier(nn.Module):
-    def __init__(self, input_size, hidden_size, embedding_dim, num_classes, dropout_prob=0.5):
+    def __init__(self, input_size, hidden_size, embedding_dim, num_classes, dropout=0.4):
         super(IntentClassifier, self).__init__()
         self.embedding = nn.Embedding(input_size, embedding_dim)
+        self.dropout1 = nn.Dropout(dropout)
         self.rnn = nn.LSTM(embedding_dim, hidden_size, batch_first=True, bidirectional=True)
-        self.dropout = nn.Dropout(dropout_prob)  # Lớp Dropout để giảm overfitting
-        self.fc = nn.Linear(hidden_size * 2, num_classes)  # Nhân đôi hidden_size vì BiLSTM có 2 hướng
+        self.dropout2 = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_size * 2, num_classes)  # *2 vì có 2 hướng
 
     def forward(self, x):
-        # x: [batch_size, seq_length]
-        embedded = self.embedding(x)      # [batch_size, seq_length, embedding_dim]
-        _, (hidden, _) = self.rnn(embedded)
-        # hidden[-2:] chứa cả 2 hướng (forward và backward)
-        hidden = torch.cat((hidden[-2], hidden[-1]), dim=1)  # [batch_size, hidden_size * 2]
-        hidden = self.dropout(hidden)  # Áp dụng Dropout
-        output = self.fc(hidden)           # [batch_size, num_classes]
+        # x: [batch_size, seq_len]
+        embedded = self.embedding(x)
+        embedded = self.dropout1(embedded)
+        _, (hidden, _) = self.rnn(embedded)  # hidden: [2, batch_size, hidden_size] - 2 là do bidirectional
+        # Kết hợp hidden state từ cả hai hướng
+        hidden = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1)
+        hidden = self.dropout2(hidden)
+        output = self.fc(hidden)      # [batch_size, num_classes]
         return output
